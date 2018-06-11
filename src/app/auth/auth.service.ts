657caacc64d8e79e89ac48f49ff88069dc0fb272
Petar <petarmarjanovic12@yahoo.com>
2018-06-11 19:33:08 +0200

    Changed bg, added no awards msg, deleted user image, fixed finished btn width, added routing when logged in

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,34 +1,38 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
 
   authState: any = null;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
     this.afAuth.authState.subscribe((auth) => {
       this.authState = auth
+      if (auth) {
+        this.router.navigate(['/home/dashboard']);
+      }
     });
   }
 
   get isUserAnonymousLoggedIn(): boolean {
     return (this.authState !== null) ? this.authState.isAnonymous : false
   }
- 
+
   get currentUserId(): string {
     return (this.authState !== null) ? this.authState.uid : ''
   }
- 
+
   get currentUserName(): string {
     return this.authState['email']
   }
- 
+
   get currentUser(): any {
     return (this.authState !== null) ? this.authState : null;
   }
- 
+
   get isUserEmailLoggedIn(): boolean {
     if ((this.authState !== null) && (!this.isUserAnonymousLoggedIn)) {
       return true
@@ -36,7 +40,7 @@ export class AuthService {
       return false
     }
   }
- 
+
   signUpWithEmail(email: string, password: string) {
     return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
       .then((user) => {
@@ -47,7 +51,7 @@ export class AuthService {
         throw error
       });
   }
- 
+
   loginWithEmail(email: string, password: string) {
     return this.afAuth.auth.signInWithEmailAndPassword(email, password)
       .then((user) => {
@@ -65,7 +69,7 @@ export class AuthService {
   denied(state: boolean) {
     this.stateSource.next(state)
   }
- 
+
   signOut(): void {
     this.afAuth.auth.signOut();
   }
